2ffffb9c384cc5f052556a64683f7dae1537de18
harshgabani <125270808+harshgabani@users.noreply.github.com>
2023-05-15 14:08:49 +0530

    Add files via upload

---
+++ Examples/grade/Grade.java
@@ -0,0 +1,40 @@
+package grade;
+import java.util.*;
+
+public class Grade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        
+        int sum = n1+n2+n3;
+        int avg = sum/3;
+        
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + avg);
+        
+        int num = avg / 10;
+        
+        switch (num) {
+            case 10:
+            	System.out.println("Grade: A");
+                break;
+            case 9:
+                System.out.println("Grade: A");
+                break;
+            case 8:
+                System.out.println("Grade: B");
+                break;
+            case 7:
+                System.out.println("Grade: C");
+                break;
+            default: 
+                System.out.println("Grade: D");
+        }
+
+	}
+
+}
